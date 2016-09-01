0b1bd74efd639e6ce3c1cdecadcdb6b4cd1e77dd
Peter Hodgkinson <phodgkinson@signiant.com>
2016-09-01 13:31:29 -0400

    -Refactor to recusrively deal with sub-objects in the schema

--- app/helpers/printSchemaReference.js
+++ app/helpers/printSchemaReference.js
@@ -2,28 +2,40 @@ var Handlebars = require("handlebars");
 var common = require("../lib/common");
 // var entities = require("entities");
 
+var formatModel = function(model, options) {
+    if (typeof model === 'object' && typeof model.properties === 'object')
+        model = model.properties;
+    
+    var clonedModel = JSON.parse(JSON.stringify(model));
+    Object.keys(clonedModel).forEach(function(propName) {
+        var prop = clonedModel[propName];
+        if (prop.type) {
+            if (prop.type !== "object") {
+                clonedModel[propName] = prop.type;
+            } else {
+                clonedModel[propName] = formatModel(prop, options)
+            }
+        }
+            
+        if (prop.format) {
+            clonedModel[propName] += ('(' + prop.format + ')');
+        }
+    })
+    
+    if (options.hash.type == 'array')
+        clonedModel = [clonedModel];
+    
+    return clonedModel;
+}
+
 module.exports = function(reference, options) {
   if (!reference) {
     console.error("Cannot print null reference.");
     return '';
   }
   var model = common.resolveSchemaReference(reference, options.data.root);
-  if (typeof model === 'object' && typeof model.properties === 'object')
-    model = model.properties;
-    
-  var clonedModel = JSON.parse(JSON.stringify(model));
-  Object.keys(clonedModel).forEach(function(propName) {
-    var prop = clonedModel[propName];
-    if (prop.type) {
-        clonedModel[propName] = prop.type;
-      if (prop.format) {
-          clonedModel[propName] += ('(' + prop.format + ')');
-      }
-    }
-  })
-  if (options.hash.type == 'array')
-      clonedModel = [clonedModel];
-  var html = common.printSchema(clonedModel);
+    model = formatModel(model, options);
+  var html = common.printSchema(model);
   // html = common.highlight(html, 'json')
   // html = entities.decodeHTML(html); html;
   return new Handlebars.SafeString(html);
