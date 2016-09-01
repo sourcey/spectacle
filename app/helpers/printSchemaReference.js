9847e9159f3ee474b45b2202b7a21b12bef74026
Peter Hodgkinson <phodgkinson@signiant.com>
2016-09-01 13:00:00 -0400

    -Prevent mutation of the original spec object properties by cloning the model before change the properties.

--- app/helpers/printSchemaReference.js
+++ app/helpers/printSchemaReference.js
@@ -10,18 +10,20 @@ module.exports = function(reference, options) {
   var model = common.resolveSchemaReference(reference, options.data.root);
   if (typeof model === 'object' && typeof model.properties === 'object')
     model = model.properties;
-  Object.keys(model).forEach(function(propName) {
-    var prop = model[propName];
+    
+  var clonedModel = JSON.parse(JSON.stringify(model));
+  Object.keys(clonedModel).forEach(function(propName) {
+    var prop = clonedModel[propName];
     if (prop.type) {
-      model[propName] = prop.type;
+        clonedModel[propName] = prop.type;
       if (prop.format) {
-        model[propName] += ('(' + prop.format + ')');
+          clonedModel[propName] += ('(' + prop.format + ')');
       }
     }
   })
   if (options.hash.type == 'array')
-    model = [model];
-  var html = common.printSchema(model);
+      clonedModel = [clonedModel];
+  var html = common.printSchema(clonedModel);
   // html = common.highlight(html, 'json')
   // html = entities.decodeHTML(html); html;
   return new Handlebars.SafeString(html);
