947c6bbb9b73f637b6230fb694afa671f5a990ca
Kam <auscaster@gmail.com>
2016-11-25 00:46:14 +0100

    Fix #12 response examples parsing schema types

--- app/helpers/printSchemaReference.js
+++ app/helpers/printSchemaReference.js
@@ -2,39 +2,26 @@ var Handlebars = require('handlebars');
 var common = require('../lib/common');
 var _ = require('lodash');
 
-var formatModel = function(model, options) {
-    if (typeof model === 'object' && typeof model.properties === 'object')
-        model = model.properties;
-
-    var clonedModel = _.cloneDeep(model);
-    Object.keys(clonedModel).forEach(function(propName) {
-        var prop = clonedModel[propName];
-        if (prop.type) {
-            if (prop.type !== "object") {
-                clonedModel[propName] = prop.type;
-            } else {
-                clonedModel[propName] = formatModel(prop, options)
-            }
-        }
-
-        if (prop.format) {
-            clonedModel[propName] += ('(' + prop.format + ')');
-        }
-    })
-
-    if (options.hash.type == 'array')
-        clonedModel = [clonedModel];
-
-    return clonedModel;
-}
-
 module.exports = function(reference, options) {
   if (!reference) {
     console.error("Cannot print null reference.");
     return '';
   }
   var model = common.resolveSchemaReference(reference, options.data.root);
-  var formatted = formatModel(model, options);
-  var html = common.printSchema(model);
+  if (typeof model === 'object' && typeof model.properties === 'object')
+    model = model.properties;
+  var cloned = _.cloneDeep(model);
+  Object.keys(cloned).forEach(function(propName) {
+    var prop = cloned[propName];
+    if (prop.type) {
+      cloned[propName] = prop.type;
+      if (prop.format) {
+        cloned[propName] += ('(' + prop.format + ')');
+      }
+    }
+  })
+  if (options.hash.type == 'array')
+    cloned = [cloned];
+  var html = common.printSchema(cloned);
   return new Handlebars.SafeString(html);
 };
