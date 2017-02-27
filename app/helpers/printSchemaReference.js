e0c951ff9f6e6956de6fd690b955a901709abdc6
Al James <al@ometria.com>
2017-02-27 15:08:17 +0000

    Tidy up fix and remove original code

--- app/helpers/printSchemaReference.js
+++ app/helpers/printSchemaReference.js
@@ -8,44 +8,16 @@ module.exports = function(reference, options) {
     return '';
   }
   var model = common.resolveSchemaReference(reference, options.data.root);
-  if (typeof model === 'object' && typeof model.properties === 'object')
-    model = model.properties;
-  var cloned = _.cloneDeep(model);
-  Object.keys(cloned).forEach(function(propName) {
-    var prop = cloned[propName];
-    if (prop.type) {
-      cloned[propName] = prop.type;
-      if (prop.format) {
-        cloned[propName] += ('(' + prop.format + ')');
-      }
-    }
-  })
-  if (options.hash.type == 'array')
-    cloned = [cloned];
-  var html = common.printSchema(cloned);
-  return new Handlebars.SafeString(html);
-};
-
-
-var Handlebars = require('handlebars');
-var common = require('../lib/common');
-var _ = require('lodash');
-
-module.exports = function(reference, options) {
-  if (!reference) {
-    console.error("Cannot print null reference.");
-    return '';
-  }
-  var model = common.resolveSchemaReference(reference, options.data.root);
   if (typeof model === 'object' && typeof model.properties === 'object') {
-    if (model['example']) {
+    var cloned;
+    if (model.example) {
       // Use the supplied example
       model = model.example;
-      var cloned = _.cloneDeep(model);
+      cloned = _.cloneDeep(model);
     } else {
       // Create json object of keys : type info string
       model = model.properties;
-      var cloned = _.cloneDeep(model);
+      cloned = _.cloneDeep(model);
       Object.keys(cloned).forEach(function(propName) {
         var prop = cloned[propName];
         if (prop.type) {
