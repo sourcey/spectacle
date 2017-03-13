20fce13763064999ca7ea94f06a5fb1ee4ba0f78
Ryan Leonard <rhinoisnot@gmail.com>
2017-03-13 08:16:52 -0700

    Added basic testing of the preprocessor.  Setup Mocha.

---
+++ test/test-preprocessor.js
@@ -0,0 +1,35 @@
+var chai = require("chai");
+var should = chai.should();
+
+var preprocessor = require("../app/lib/preprocessor");
+
+var minimal = require("./minimal");
+
+describe("preprocessor", function() {
+
+  var spec = null;
+  var processed = null;
+
+  beforeEach(function() {
+    spec = Object.assign({}, minimal);
+    processed = preprocessor({}, spec);
+  });
+
+  describe("with minimal spec", function() {
+
+    it("should retain initial values", function() {
+      Object.assign({}, processed, minimal).should.deep.equal(processed);
+    });
+
+    it("should add 'tags'", function() {
+      processed.should.have.property("tags");
+      processed.tags.should.deep.equal([]);
+    });
+
+    it("should add 'showTagSummary'", function() {
+      processed.should.have.property("showTagSummary", false);
+    });
+
+  });
+
+});
