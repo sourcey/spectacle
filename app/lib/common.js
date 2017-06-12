37687be20e55729b78959b6999315f04c9f0089d
Josa Gesell <josa@gesell.me>
2017-06-12 10:08:56 +0200

    Hide readOnly attributes
    Fixes #59

--- app/lib/common.js
+++ app/lib/common.js
@@ -67,31 +67,32 @@ var common = {
   //   return cloned;
   // },
 
-  formatExample: function(value, root) {
+  formatExample: function(value, root, options) {
     if (!value) {
       // throw 'Cannot format NULL object ' + value;
       return;
     }
-
+    
   	if (value.example) {
   	  return value.example;
   	}
     else if (value.schema) {
-      return this.formatExampleProp(value.schema, root);
+      return this.formatExampleProp(value.schema, root, options);
     }
     else if (value.type || value.allOf)  {
-      return this.formatExampleProp(value, root);
+      return this.formatExampleProp(value, root, options);
     }
 
     console.error('Cannot format object ', value);
   },
 
-  formatExampleProp: function(ref, root) {
+  formatExampleProp: function(ref, root, options) {
     if (!ref) {
       // throw 'Cannot format NULL property ' + ref;
       return;
     }
-
+    
+    var showReadOnly = options.showReadOnly !== false
     var that = this;
   	
     if (ref.example) {
@@ -99,24 +100,26 @@ var common = {
     }
     else if (ref.$ref) {
   	  ref = this.resolveSchemaReference(ref.$ref, root);
-  	  return this.formatExampleProp(ref, root);
+  	  return this.formatExampleProp(ref, root, options);
   	}
     else if (ref.properties && ref.type == 'object') {
       var obj = {};
       Object.keys(ref.properties).forEach(function(k) {
-        obj[k] = that.formatExampleProp(ref.properties[k], root);
+        if (showReadOnly || ref.properties[k].readOnly !== true) {
+          obj[k] = that.formatExampleProp(ref.properties[k], root, options);
+        }
       });
       return obj;
     }
     else if (ref.allOf) {
       var obj = {};
       ref.allOf.forEach(function(parent) {
-        obj = Object.assign(that.formatExampleProp(parent, root), obj);
+        obj = Object.assign(that.formatExampleProp(parent, root, options), obj);
       });
       return obj;
     }
   	else if (ref.items && ref.type == 'array') {
-  	  return [ this.formatExampleProp(ref.items, root) ];
+  	  return [ this.formatExampleProp(ref.items, root, options) ];
   	}
     else if (ref.type) {
   	  return ref.type + (ref.format ? ' (' + ref.format + ')' : '');
