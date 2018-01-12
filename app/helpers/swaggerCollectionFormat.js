7ec345e4642c21f303e3cb68ad6d23aead339f89
Vlad Tsepelev <reg.yeti@gmail.com>
2018-01-12 02:22:24 +0100

    Update swaggerCollectionFormat.js

--- app/helpers/swaggerCollectionFormat.js
+++ app/helpers/swaggerCollectionFormat.js
@@ -1,9 +1,9 @@
 module.exports = function(value, paramName) {
   return {
-    'csv': 'comma separated (`' + paramName + '=aaa,bbb`)',
-    'ssv': 'space separated (`' + paramName + '=aaa bbb`)',
-    'tsv': 'tab separated (`' + paramName + '=aaa\\tbbb`)',
-    'pipes': 'pipe separated (`' + paramName + '=aaa|bbb`)',
-    'multi': 'multiple parameters (`' + paramName + '=aaa&' + paramName + '=bbb`)'
+    'csv': 'Array values passed separated by comma: `?' + paramName + '=aaa,bbb`',
+    'ssv': 'Array values passed separated by space: `?' + paramName + '=aaa bbb`',
+    'tsv': 'Array values passed separated by tab: `?' + paramName + '=aaa\\tbbb`',
+    'pipes': 'Array values passed separated by pipe: `?' + paramName + '=aaa|bbb`',
+    'multi': 'Array values passed as multiple parameters: `?' + paramName + '=aaa&' + paramName + '=bbb`'
   }[value]
 };
