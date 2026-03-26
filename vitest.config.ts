674f0882fd2a79180d5c971f95e4da34a1a762c8
Kam <hello@sourcey.com>
2026-03-26 14:11:11 +1000

    release: publish sourcey 3.3.5

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     include: ["test/**/*.test.ts"],
+    testTimeout: 20000,
     coverage: {
       provider: "v8",
       include: ["src/**/*.ts"],
